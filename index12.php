f00dc877c6f7646e4cc9b1f69c7827ad4afe3dd8
evanbeaulieu <evanbeaulieu199715@gmail.com>
2017-04-16 20:50:26 +0100

    Add files via upload

--- index12.php
+++ index12.php
@@ -1,11 +1,10 @@
-﻿<?php // This snippet of php should go on the top of all of your pages. If you are not logged in, you'll be redirected to the home page.
+<?php // This snippet of php should go on the top of all of your pages. If you are not logged in, you'll be redirected to the home page.
 session_start();
 if(!$_SESSION['loggedIn']) {
   header("location:index1.php"); 
   die(); 
 }
 ?>
-
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -45,8 +44,8 @@ if(!$_SESSION['loggedIn']) {
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#home">HOME</a></li>
                     
-                    <li><a href="#task">UPLOAD</a></li>
-                     <li><a href="#about">TASKS</a></li>
+                    <li><a href="#portfolio">UPLOAD</a></li>
+                     <li><a href="#about">ABOUT</a></li>
                      <li><a href="#contact">CONTACT</a></li>
 					 <li><a href="logout.php">LOG OUT</a></li>
 					 
@@ -77,15 +76,15 @@ if(!$_SESSION['loggedIn']) {
         </div>
        </section>
     <!--END SECTION-->
-     <!--task SECTION-->
-     <section id="task"  >
+     <!--PORTFOLIO SECTION-->
+    <section id="portfolio"  >
             <div class="container">
                     <div class="row text-center" >
                <div class="col-md-12">
                     <h1>Upload Documents Here </h1>
 					<br></br>
 					<br></br>
-		<form action="upload.php" method="post" enctype = "multipart/form-data">
+		<form action="upload.php" method="post">
 	<div class="form-group">
 			<label for="tasktitle">Task Title</label>
     <input type="task" class="form-control" name="task_title"  placeholder="Enter task title">
@@ -123,57 +122,6 @@ if(!$_SESSION['loggedIn']) {
 						<option>pdf</option>
 					</select>
 			</div>
-			
-		
-		<div>	
-		<ul class="checkbox-grid" style="list-style-type: none;">
-		
-				<li><input type="checkbox" name="task_tags[]" value="Sociology" onclick='chkcontrol(0)'; >Sociology</li>
-				<li><input type="checkbox" name="task_tags[]" value="Politics" onclick='chkcontrol(1)'; >Politics</li>
-				<li><input type="checkbox" name="task_tags[]" value="History" onclick='chkcontrol(2)'; >History</li>
-				<li><input type="checkbox" name="task_tags[]" value="Mathematics" onclick='chkcontrol(6)'; >Mathematics</li>
-			
-				
-			
-				<li><input type="checkbox" name="task_tags[]" value="Prose" onclick='chkcontrol(3)'; >Prose</li>
-				<li><input type="checkbox" name="task_tags[]" value="Poetry" onclick='chkcontrol(4)'; >Poetry</li>
-				<li><input type="checkbox" name="task_tags[]" value="Languages" onclick='chkcontrol(5)'; >Languages</li>
-				<li><input type="checkbox" name="task_tags[]" value="Media" onclick='chkcontrol(6)'; >Media</li>
-			
-			
-			
-				<li><input type="checkbox" name="task_tags[]" value="Philosophy" onclick='chkcontrol(0)'; >Philosophy</li>
-				<li><input type="checkbox" name="task_tags[]" value="Physics" onclick='chkcontrol(1)'; >Physics</li>
-				<li><input type="checkbox" name="task_tags[]" value="Chemistry" onclick='chkcontrol(2)'; >Chemistry</li>
-				<li><input type="checkbox" name="task_tags[]" value="Programming" onclick='chkcontrol(6)'; >Programming</li>
-			
-				
-			
-				<li><input type="checkbox" name="task_tags[]" value="Biology" onclick='chkcontrol(3)'; >Biology</li>
-				<li><input type="checkbox" name="task_tags[]" value="Astrology" onclick='chkcontrol(4)'; >Astrology</li>
-				<li><input type="checkbox" name="task_tags[]" value="Law" onclick='chkcontrol(5)'; >Law</li>
-				<li><input type="checkbox" name="task_tags[]" value="Accounting" onclick='chkcontrol(6)'; >Accounting</li>
-			
-			
-			
-				<li><input type="checkbox" name="task_tags[]" value="Economics" onclick='chkcontrol(0)'; >Economics</li>
-				<li><input type="checkbox" name="task_tags[]" value="Management" onclick='chkcontrol(1)'; >Management</li>
-				<li><input type="checkbox" name="task_tags[]" value="Taxation" onclick='chkcontrol(2)'; >Taxation</li>
-				<li><input type="checkbox" name="task_tags[]" value="Networking" onclick='chkcontrol(6)'; >Networking</li>
-			
-				
-			
-				<li><input type="checkbox" name="task_tags[]" value="Software Design" onclick='chkcontrol(3)'; >Software Design</li>
-				<li><input type="checkbox" name="task_tags[]" value="Hardware Design" onclick='chkcontrol(4)'; >Hardware Design</li>
-				<li><input type="checkbox" name="task_tags[]" value="App Design" onclick='chkcontrol(5)'; >App Design</li>
-				<li><input type="checkbox" name="task_tags[]" value="Website Design" onclick='chkcontrol(6)'; >Website Design</li>
-			
-			
-			</ul>
-				
-				
-		
-			</div>
   
 			<div class="form-group row">
 				<label for="example-date-input" class="col-2 col-form-label">Date for task to be claimed</label>
@@ -187,58 +135,55 @@ if(!$_SESSION['loggedIn']) {
 						<input class="form-control" type="date"  name="enddate">
 			</div>
  
-			  <br>
-					<label class="custom-file-upload">
-						<input type="file" name="doc"/>
-					</label>
-		
-				<input type="submit" name="submit">  
-<p>Please upload your document to be reviewed/corrected</p>
-			 </div>
+			<input type="submit">    </form>
+					
+               </div>
                      </div>
 					 </div>
 					  </div>
 					   </div>
 					   </div>
 					    </div>
-							</section>
-			</form>
+						
+         
 
-
-
+        </section>
+		
+	<label class="custom-file-upload">
+    <input type="file"/>
+</label>
+<p>Please upload your document to be reviewed/corrected</p>
 		
 		
 		
-
+    <!--END PORTFOLIO SECTION-->
 		
-		
-		
-    <!--END task SECTION-->
-		
-		
-		
-    <!--END task SECTION-->
-    <!--ABOUT SECTION-->
-	<br></br>
-	<br></br>
-	
-        <section id="about"  >
-            <div class="container">
-                 <div class="row text-center" >
-               <div class="col-md-12">
-                    <h1>TASKS </h1>
-					<br><br>
-               </div>
-			   <iframe src="http://localhost/tasks.php"  
-		style="width:100%;
+		<iframe src="http://localhost/tasks.php" class="task-frame" 
+		style=" width:100%;
  float:center;
  height:100%;
  padding:10px;
  position:relative;
  overflow-x:hidden;
  overflow-y:visible;
- border:none;
- background-color:white;" >Tasks</iframe>
+ border:none;" >Tasks</iframe>
+		
+		
+		
+		
+    <!--END PORTFOLIO SECTION-->
+    <!--ABOUT SECTION-->
+	<br></br>
+	<br></br>
+	<br></br>
+	<br></br>
+	<br></br>
+        <section id="about"  >
+            <div class="container">
+                 <div class="row text-center" >
+               <div class="col-md-12">
+                    <h1>ABOUT  COMPANY</h1>
+               </div>
                      </div>
           </section> 
     <!--END ABOUT SECTION-->
@@ -249,22 +194,46 @@ if(!$_SESSION['loggedIn']) {
                <div class="col-md-12">
                     <h1>CONTACT HERE</h1>
                </div>
-			   
-		
-		
-		
                      </div>
 	
-           
+           <div class="row text-center pad-top" >
+            <div class="col-md-8 col-md-offset-2">
+                <div class="row ">
+                             <form>
+                            <div class="col-md-12 ">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" required="required" placeholder="Name" />
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" required="required" placeholder="Email address" />
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" required="required" placeholder="Notes" />
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                                                   <button type="submit" class="btn btn-primary btn-lg">SUBMIT REQUEST</button>
+
+                            </div>
+                                 </form>
+                        </div>
+            </div>
+               
+               </div>
         </div>
 		<h3> How to reach us! </h3>
 
-<pre>
- </p>
-          
-          <div class="add"><i>Number:</i>061 202368
-Email:</i>15142151@studentmail.ie
- <i>Address:</i>B2005,Main Building, University of Limerick, Ireland
+<pre>Number:061 202368
+Email: 15142151@studentmail.ie
+Address: B2005,Building, University Of Limerick </p>
+        </section>
+     <section>
+          <div class="add">
+ <i>Address: </i> Students Union, Univeristy of Limerick, Ireland
    </div>
       <h3> Our Location </h3>
 
--- login.php
+++ login.php
@@ -3,17 +3,27 @@ session_start();
 include 'dbh.php';
 $email = $_POST['email'];
 $pwd = $_POST['pwd'];
-$sql = "SELECT * FROM user1 WHERE email='$email' AND pwd='$pwd'";
+
+//Cant query on pwd because this is the unhashed password. We can query on email because it will be unique.
+$sql = "SELECT * FROM user1 WHERE email='$email'";
 $result = $conn->query($sql);
-if (!$row = $result->fetch_assoc()) {
-	echo "Your email or password is incorrect!";
-	echo "<br>";
-	echo "<br>";
-	echo("<button onclick=\"location.href='index1.php'\">BACK</button>");
-} else{
-	 header("Location: index12.html"); 
-    exit;
+
+if($result->num_rows > 0) {
+    // output data of each row
+    $row = mysqli_fetch_assoc($result);
+    $hashedpwd = $row['pwd'];
+    if(password_verify($pwd, $hashedpwd)){//password_verify verifies the password against the hashed password
+    $_SESSION['loggedIn'] = true;
+    header("Location: index12.php"); 
+    exit;  
+    }
 }
+
+echo "Your email or password is incorrect!";
+echo "<br>";
+echo "<br>";
+echo("<button onclick=\"location.href='index1.php'\">BACK</button>"); 
+
 //header("Location: index1.php");
 ?>
 
